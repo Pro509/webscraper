d8ad48e7d2be333e0a31af316414931b663e532a
Pritish Dugar <pritishdugar509@gmail.com>
2021-03-27 13:40:29 +0700

    functional review scraper v1

--- reviewscraper.py
+++ reviewscraper.py
@@ -1,14 +1,16 @@
 import requests
+import math
 import json
 from random import randint
 from time import sleep
 from bs4 import BeautifulSoup as soup
+from progress.bar import Bar, IncrementalBar
 
 city_url = "https://www.tripadvisor.com/Hotels-g293916-Bangkok-Hotels.html"
 tripadvisor_url = "https://www.tripadvisor.com"
 reviews = 15
 city_page = requests.get(city_url)
-print("status", city_page.status_code)
+print(f"Page Status: {city_page.status_code}")
 
 bs_obj = soup(city_page.content, 'html.parser')
 
@@ -24,7 +26,9 @@ def bubbleRatingVal(tags):
 def makeReviewPageLinks(bs_obj, reviews = 10):
     """ TripAdvisor review pages links for city-specific hotels """
     hotel_links = []
-    for link in bs_obj.findAll('a', {'class': 'review_count'}):
+    hotelsReviews = bs_obj.findAll('a', {'class': 'review_count'})
+    bar = Bar('Generating Hotel Links', max = len(hotelsReviews))
+    for link in hotelsReviews:
         # print(type(link))
         review_count = StrToInt(link.get_text().split()[0])
 
@@ -38,48 +42,51 @@ def makeReviewPageLinks(bs_obj, reviews = 10):
                 hotel_links.append(next_page)
         else:
             pass
-
+        bar.next()
+    bar.finish()
     return hotel_links
 
 def getAvgReview():
     pass
 
-city_dict = dict()
+def reviewExtract(hotel_links):
+    city_dict = dict()
+    bar = IncrementalBar('Generating Reviews', max = len(hotel_links), suffix='%(percent)d%%')
+    for link in hotel_links:
+        # print(link)
+        review_page = requests.get(link)
+        sleep(randint(1,4))
+        reviews = soup(review_page.content, 'html.parser')
 
-hotel_links = makeReviewPageLinks(bs_obj, reviews)
+        # Saving hotel name to append page reviews appropriately
+        hotel_name = reviews.find('h1').get_text()
+        if hotel_name in city_dict:
+            ids = city_dict[hotel_name]['reviews'].keys()
+            counter = int(list(ids)[-1]) + 1
+        else:
+            city_dict[hotel_name] = {'reviews': {}}
+            counter = 1
+        # print(hotel_name, '\n')
+        
+        for r in reviews.findAll('div', {'data-test-target':'HR_CC_CARD'}):
+            # print(r,'\n')
+            review_text = r.find('q').get_text() #r.span.text.strip()
+            bubble = r.find('div', {'data-test-target':'review-rating'})
+            rating = bubbleRatingVal(bubble)
+            # print(rating)
+            city_dict[hotel_name]['reviews'].update({f'{counter}': {'review_text': review_text, 'rating': rating}})
+            counter += 1
+            # print(review_text, '\n')
+        bar.next()
+    bar.finish()
+    return city_dict
 
+if __name__=="__main__":
+    hotel_links = makeReviewPageLinks(bs_obj, reviews)
+    data = reviewExtract(hotel_links[:3])
+    # print()
+    # print(json.dumps(city_dict, indent=4, sort_keys=True))
 
-for link in hotel_links[:int(reviews/3)]:
-    # print(link)
-    review_page = requests.get(link)
-    sleep(randint(1,4))
-    reviews = soup(review_page.content, 'html.parser')
-
-    # Saving hotel name to append page reviews appropriately
-    hotel_name = reviews.find('h1').get_text()
-    if hotel_name in city_dict:
-        ids = city_dict[hotel_name]['reviews'].keys()
-        counter = int(list(ids)[-1]) + 1
-    else:
-        city_dict[hotel_name] = {'reviews': {}}
-        counter = 1
-    print(hotel_name, '\n')
-    
-    for r in reviews.findAll('div', {'data-test-target':'HR_CC_CARD'}):
-        # print(r,'\n')
-        review_text = r.find('q').get_text() #r.span.text.strip()
-        bubble = r.find('div', {'data-test-target':'review-rating'})
-        rating = bubbleRatingVal(bubble)
-        # print(rating)
-        city_dict[hotel_name]['reviews'].update({f'{counter}': {'review_text': review_text, 'rating': rating}})
-        counter += 1
-        # print(review_text, '\n')
-
-
-
-# print()
-# print(json.dumps(city_dict, indent=4, sort_keys=True))
-
-with open('data.json', 'w') as outfile:
-    json.dump(city_dict, outfile, indent=4)
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
 
